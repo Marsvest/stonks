b61a531047aa61e01232253b721d3251c207394d
Marsvest <alajh.akbar@gmail.com>
2024-11-15 17:32:07 +1000

    обновление модели (токенизация русского текста)

--- model.py
+++ model.py
@@ -1,25 +1,35 @@
+import warnings
 import torch
 import os
-from transformers import AutoTokenizer, AutoModel
+from transformers import BertModel, BertTokenizer
 from safetensors.torch import save_file, load_file
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 
 
-# KEY: Percent increase or decrease of stock function after this news items
-# VALUE: Tensor of news items linked to this price
+# Использовать на ТОЛЬКО НА ПРОДЕ (блокирует warning'и)
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
 class SimModel:
     def __init__(self, path_to_model: str = "model.safetensors") -> None:
-        model_name = "sentence-transformers/all-MiniLM-L6-v2"
-        self._tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self._transformer_model = AutoModel.from_pretrained(model_name)
+        model_name = "DeepPavlov/rubert-base-cased"
+        self._tokenizer = BertTokenizer.from_pretrained(model_name)
+        self._transformer_model = BertModel.from_pretrained(
+            model_name
+        )  # Use BertModel for embeddings
 
         self._load_embeddings(path_to_model)
 
     def _vectorize(self, text: str) -> torch.Tensor:
         inputs = self._tokenizer(
-            text, return_tensors="pt", padding=True, truncation=True
+            text,
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=512,
         )
+
         with torch.no_grad():
             outputs = self._transformer_model(**inputs)
         embeddings = outputs.last_hidden_state.mean(dim=1)
@@ -45,10 +55,13 @@ class SimModel:
         input_embedding = self._vectorize(text)
 
         max_similarity = float(-1)
+        predict_price = None
         for price, embedding in tqdm(self.embeddings.items()):
-            similarity = cosine_similarity(input_embedding, embedding).item()
+            similarity = cosine_similarity(
+                input_embedding.numpy().reshape(1, -1), embedding.numpy().reshape(1, -1)
+            ).item()
             if similarity > max_similarity:
                 max_similarity = similarity
                 predict_price = price
 
-        return float(predict_price)
+        return float(predict_price) if predict_price is not None else 0.0
